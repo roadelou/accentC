448c993366f66278dbdd614356786e0fb038b5c2
roadelou <tableau.roche@gmail.com>
2021-08-27 18:28:55 +0200

    Added new `match` function with MatchList struct.

--- include/accent.h
+++ include/accent.h
@@ -13,7 +13,8 @@
 
 /********************************** INCLUDES **********************************/
 
-/* The includes for your header go here */
+// Used for size_t.
+#include <stddef.h>
 
 /*********************************** MACROS ***********************************/
 
@@ -27,7 +28,23 @@
 
 /* The enums of your header go here */
 
-/* The structs of your header go here */
+// Description
+// ===========
+// This struct is used by the "match" function to report the number and
+// locations of matches of a substring within a string.
+//
+// Fields
+// ======
+//  - count: The number of matches in the string, also the length of the
+//  "position" array.
+//  - position: A dynamically allocated array containing the positions of the
+//  matched substring within the string. The return index corresponds to the
+//  offset of the beginning of the match from the beginning of the string.
+//
+struct MatchList {
+    size_t count;
+    size_t *position;
+};
 
 /* The unions of your header go here */
 
@@ -54,6 +71,34 @@
 // replaced with `word`.
 //
 char *replace(const char *text, const char *pattern, const char *word);
+
+// Description
+// ===========
+// This function is used to find all occurences of `pattern` within `text`.
+//
+// Arguments
+// =========
+//  - text: The string in which the `pattern` should be found one or more times.
+//  Must be null-terminated.
+//  - pattern: The substring which should be looked for in the `text`. Should be
+//  null-terminated.
+//
+// Returns
+// =======
+// A MatchList struct containing a list of all the position where the `pattern`
+// is found in the `text`.
+//
+struct MatchList match(const char *text, const char *pattern);
+
+// Description
+// ===========
+// Helper function to free the content of a MatchList object.
+//
+// Arguments
+// =========
+//  - match_list: The match_list object whose content should be freed.
+//
+void match_list_free(struct MatchList *match_list);
 
 /* End of include once header guard */
 #endif
--- src/accent.c
+++ src/accent.c
@@ -98,4 +98,68 @@ char *replace(const char *text, const char *pattern, const char *word) {
     return returned_string;
 }
 
+struct MatchList match(const char *text, const char *pattern) {
+    // A cursor for our current position in the original `text`.
+    const char *cursor = text;
+    //
+    // The length (in bytes) of the `pattern`, computed once.
+    const size_t pattern_length = strlen(pattern);
+    //
+    // A compile time constant which tells us how many positions can be held in
+    // one allocated chunk.
+    const size_t pos_per_chunk = ACCENT_CHUNK_SIZE / sizeof(size_t);
+    //
+    // The MatchList object we intend to return.
+    struct MatchList match_list;
+    //
+    // We initialize the MatchList.
+    match_list.count = 0;
+    match_list.position = NULL;
+    //
+    // We go over all the possible positions of the `text` to try and find the
+    // pattern. Note that strncmp is aware of null-terminating bytes and won't
+    // read beyond them, avoiding segmentation faults and out-of bounds reads.
+    while (*cursor != '\0') {
+        //
+        // For each position of the `cursor`, we check if it matches the
+        // `pattern`.
+        if (strncmp(cursor, pattern, pattern_length) == 0) {
+            // We have found an occurence of the `pattern`. We first update the
+            // count of the MatchList.
+            match_list.count++;
+            //
+            // We check whether we must allocate more memory for our array of
+            // positions. This will happen only when the number of occurences is
+            // one more than can be held in a ACCENT_CHUNK_SIZE (for instance
+            // 1). Using a division here works better because it will account
+            // for cases where ACCENT_CHUNK_SIZE is not a multiple of
+            // sizeof(size_t).
+            if (match_list.count % (pos_per_chunk) == 1) {
+                // We assume malloc never fails here. Also note that
+				// `match_list.count / pos_per_chunk` is the number of chunks
+				// already allocated.
+                match_list.position = realloc(
+                    match_list.position,
+                    (1 + match_list.count / pos_per_chunk) * ACCENT_CHUNK_SIZE);
+            }
+            // At this point, the position array has enough memory allocated to
+            // hold the newly found position.
+            *(match_list.position + match_list.count - 1) =
+                (size_t)cursor - (size_t)text;
+        }
+        // We update the cursor for the next iteration.
+        cursor++;
+    }
+    // At this point we have gone over the entire text, we may return the
+    // expected MatchList.
+    return match_list;
+}
+
+void match_list_free(struct MatchList *match_list) {
+    // We set the count of values back to zero.
+    match_list->count = 0;
+    // We free the memory allocated for the positions.
+    free(match_list->position);
+}
+
 /************************************ EOF *************************************/
